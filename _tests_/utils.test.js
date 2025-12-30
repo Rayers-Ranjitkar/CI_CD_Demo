b5882dd8dace793ca4e28e4b3704b1b24091c8d9
devRayers <155600983+Rayers-Ranjitkar@users.noreply.github.com>
2025-12-30 15:13:36 +0545

    last debugging substraction

--- _tests_/utils.test.js
+++ _tests_/utils.test.js
@@ -1,14 +1,13 @@
-// import {add} from './utils.js';
 const { add, multiply,substraction } = require('../utils');
 
 test('adds 2 + 2 correctly', () => {
-  expect(add(2, 2)).toBe(4); // intentionally failing first
+  expect(add(2, 2)).toBe(4); 
 });
 
 test('multiplies 3 * 2 correctly', () => {
-  expect(multiply(3, 2)).toBe(6); // correct test case
+  expect(multiply(3, 2)).toBe(6); 
 });
 
 test('substracts 8 - 5 correctly',()=>{
-    expect(substraction(8,5).toBe(3) );
+    expect(substraction(8,5)).toBe(3);
 })
