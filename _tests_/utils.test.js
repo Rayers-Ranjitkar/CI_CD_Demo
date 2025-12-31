db05779777d235f253a7160d593254e0fc448a9b
devRayers <155600983+Rayers-Ranjitkar@users.noreply.github.com>
2025-12-31 14:18:49 +0545

    debugged utils.test.js

--- _tests_/utils.test.js
+++ _tests_/utils.test.js
@@ -1,5 +1,5 @@
 // const { add, multiply,substraction } = require('../utils');
-import { add, multiply, substraction } from "./utils.js";
+import { add, multiply } from "./utils.js";
 
 test('adds 2 + 2 correctly', () => {
   expect(add(2, 2)).toBe(4); 
