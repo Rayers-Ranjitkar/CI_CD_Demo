f2f0c38841fb304854a3a241777503dfa23c9350
devRayers <155600983+Rayers-Ranjitkar@users.noreply.github.com>
2025-12-31 15:06:48 +0545

    added next multip;y

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ import { add , multiply } from "./utils.js";
 //displaying changes in index.html
 document.getElementById("resultAdd").textContent = `2 + 3 = ${add(2, 3)}`;
 document.getElementById("resultMultiply").textContent = `2 * 3 = ${multiply(2, 3)}`;
+document.getElementById("resultMultiply2").textContent = `2 * 5 = ${multiply(2, 5)}`;
 
