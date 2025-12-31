e1ecbb179c32ad60703b7e301d495a2c60826b0e
devRayers <155600983+Rayers-Ranjitkar@users.noreply.github.com>
2025-12-31 14:29:25 +0545

    added jest environment

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import { add , multiply } from "./utils.js";
 
 //displaying changes in index.html
 document.getElementById("resultAdd").textContent = `2 + 3 = ${add(2, 3)}`;
-document.getElementById("resultMultiply").textContent = `2 + 3 = ${multiply(2, 3)}`;
+document.getElementById("resultMultiply").textContent = `2 * 3 = ${multiply(2, 3)}`;
 
